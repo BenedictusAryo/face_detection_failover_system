fd0ba9ed993ea789990123c8374500befa9cdc99
BenedictusAryo <benedictusaryo@outlook.com>
2022-05-12 16:54:58 +0700

    fix issue dsfd

--- face_detection_failover.py
+++ face_detection_failover.py
@@ -3,24 +3,19 @@ from pathlib import Path
 from typing import Any, Dict
 import numpy as np 
 import cv2
-import sys
+from yolov5facedetector.face_detector import YoloDetector
 import face_detection
 import logging
 import warnings
 warnings.filterwarnings('ignore')
-# Verification
-YOLOFACE_PATH = Path.home()/'.yoloface'
-if not YOLOFACE_PATH.exists():
-    raise FileNotFoundError(f"Path to YoloFace Model doesn't exists please download from GCS then place to ~/.yoloface")
-sys.path.append(str(YOLOFACE_PATH))
-from face_detector import YoloDetector
+
 
 # Load Model to memory
 logging.info('Load YOLOv5-Face & DSFD Model to memory')
 start = timer()
 dsfd = face_detection.build_detector(
-  "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
-yoloface = YoloDetector(weights_name='yolov5l_state_dict.pt',config_name='yolov5l.yaml',target_size=720)
+  "DSFDDetector", confidence_threshold=.65, nms_iou_threshold=.3)
+yoloface = YoloDetector(yolo_type='yolov5l', target_size=720, gpu=0)
 logging.info(f'Load model done in {timer()-start}')
 
 def correcting_rotation(face_cropped:np.array, face_keypoints):
@@ -64,14 +59,15 @@ def face_detection_failover(rgb_img:np.array)->Dict[str, Any]:
     Dict[str, Any]
         {
             'detector': 'yolo','dsfd', or 'none',
+            'rotation_degree: 0, 90, 180, or 270 degree,
             'face_img': rgb_face_img or np.nan if not detected
         }
     """    
     # Try Yolo first
     for degree in [0,1,3,2]:
         img_rot = np.rot90(rgb_img, k=degree)
-        yolo_bboxes, yolo_points = yoloface.predict(img_rot, conf_thres = 0.65)
-        yolo_bboxes, yolo_points = yolo_bboxes[0], yolo_points[0]
+        yolo_bboxes, yolo_confs, yolo_points = yoloface.predict(img_rot, conf_thres = 0.65)
+        yolo_bboxes, yolo_confs, yolo_points = yolo_bboxes[0], yolo_confs[0], yolo_points[0]
         if len(yolo_bboxes) != 0:
             bbox_idx = filter_bbox(img_rot, yolo_bboxes)
             yolo_bboxes = yolo_bboxes[bbox_idx]
@@ -84,34 +80,38 @@ def face_detection_failover(rgb_img:np.array)->Dict[str, Any]:
             face_cropped = correcting_rotation(face_cropped, yolo_points)
             return dict(
                 detector = 'yolo',
+                rotation_degree = degree*90,
                 face_img = face_cropped
             )
     else:
-        # Try DSFD
+        # Try DSFD        
         try:
-            dsfd_res = dsfd.detect(rgb_img)
-            if len(dsfd_res) != 0:
-                *dsfd_bbox, conf = dsfd_res[0]
-                xmin,ymin,xmax,ymax = np.array(dsfd_bbox).astype(int)
-                face_cropped = rgb_img[ymin:ymax, xmin:xmax]
-                # Check if face have correct output
-                if (face_cropped.shape[0] <= 0) | (face_cropped.shape[1] <= 0):
+            for degree in [0,1,3,2]:
+                img_rot = np.rot90(rgb_img, k=degree)
+                dsfd_res = dsfd.detect(img_rot)
+                if len(dsfd_res) != 0:                   
+                    bbox_idx = filter_bbox(img_rot, dsfd_res)
+                    *dsfd_bbox, conf = dsfd_res[bbox_idx]
+                    xmin,ymin,xmax,ymax = np.array(dsfd_bbox).astype(int)
+                    face_cropped = rgb_img[ymin:ymax, xmin:xmax]
+                    # Check if face have correct output
+                    if (face_cropped.shape[0] <= 0) | (face_cropped.shape[1] <= 0):
+                        continue
                     return dict(
-                        detector = 'none',
-                        face_img = np.nan
-                    )
-                return dict(
-                    detector = 'dsfd',
-                    face_img = face_cropped
-                )                
+                        detector = 'dsfd',
+                        rotation_degree = degree*90,
+                        face_img = face_cropped
+                    )                
             else:
                 return dict(
                     detector = 'none',
+                    rotation_degree = 0,
                     face_img = np.nan
                 )
         except:
             return dict(
                     detector = 'none',
+                    rotation_degree = 0,
                     face_img = np.nan
                 )
                 
